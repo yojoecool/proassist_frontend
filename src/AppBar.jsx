fb7f2bcaf482ea596f06b8cc767817e7a90084d8
yojoecool <yojoecool@gmail.com>
2019-06-01 16:20:46 -0400

    app bar, installed router

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  AppBar, Toolbar, Typography
+  AppBar, Toolbar, Typography, IconButton, Menu, MenuItem
 } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
+import MenuIcon from '@material-ui/icons/Menu';
+import useWindowDimensions from './modules/useWindowDimensions';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
   },
@@ -18,12 +15,55 @@ const useStyles = makeStyles(theme => ({
 
 function ProAssistAppBar() {
   const classes = useStyles();
+  const { width } = useWindowDimensions();
+  const mobileView = width <= 768;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = !!anchorEl;
+
+  const handleMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  }
+
+  const closeMenu = () => {
+    setAnchorEl(null);
+  }
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           ProAssist
         </Typography>
+
+        <Typography variant="subtitle1" hidden={mobileView}>
+          Test
+        </Typography>
+        <IconButton
+          aria-owns={open ? 'menu-appbar' : undefined}
+          aria-haspopup="true"
+          color="inherit"
+          hidden={mobileView}
+          onClick={handleMenu}
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={closeMenu}
+        >
+          <MenuItem onClick={closeMenu}>LogIn</MenuItem>
+          <MenuItem onClick={closeMenu}>Regsiter</MenuItem>
+        </Menu>
+
+        <IconButton
+          color="inherit"
+          hidden={!mobileView}
+        >
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
