1e2dc338438a4ad8341d7cfc6484b4aec7b76da6
Vivian Wang <vwang0101@tzuchi.us>
2019-06-09 20:40:45 -0400

    added mocked jobs

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -3,6 +3,7 @@ import { Button, Card, CardActions, CardContent, IconButton, Typography } from '
 import { makeStyles } from '@material-ui/core/styles';
 import { Close } from '@material-ui/icons';
 import classNames from 'classnames';
+import mockedJobs from '../mocks/mockedJobs';
 
 const useStyles = makeStyles({
   card: {
@@ -16,31 +17,31 @@ const useStyles = makeStyles({
 function Careers() {
   const classes = useStyles();
 
-  const [applyClicked, setApplyClicked] = React.useState(false);
+  const [selected, setSelected] = React.useState(false);
 
-  const handleApplyClicked = (e) => {
-    setApplyClicked(true);
+  const handleSelected = (job) => {
+    setSelected(true);
   };
 
   const closeDetails = (e) => {
-    setApplyClicked(false);
+    setSelected(false);
   };
 
   let description;
   let responsibilities;
   let close;
   let applyButton;
-  if (applyClicked) {
+  if (selected) {
     applyButton = <Button size="medium">Submit</Button>
     description = <div>
-      Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      {job.description}
     </div>;
     responsibilities = <div>
       Responsibilities:
       <ul>
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Lorem ipsum dolor sit amet</li>
-        <li>Lorem ipsum dolor sit amet</li>
+        {jobs.skills.map((skill, index) => {
+          return <li>{skill}</li>
+        })}
       </ul>  
     </div>;
     close = <div className={classNames('d-flex', 'justify-content-end', 'align-items-center')}>
@@ -52,32 +53,34 @@ function Careers() {
       </IconButton>
     </div>;
   } else {
-    applyButton = <Button size="medium" onClick={e => handleApplyClicked(e)}>Expand</Button>;
+    applyButton = <Button size="medium" onClick={handleSelected(job)}>Expand</Button>;
   }
 
   return (
     <div className="Careers">
       <p>Welcome to ProAssist Careers Page!</p>
-      <Card className={classes.card}>
-      <CardContent>
-        {close}
-        <Typography className={classes.title} variant="h5" color="textSecondary" gutterBottom>
-          Nuclear Pathologist Assistant
-        </Typography>
-        <Typography color="textSecondary">
-          New York City, NY
-        </Typography>
-        <Typography component="p">
-          Seeking a Nuclear Pathologist Assistant with minimal experience and MRI imaging certifications.
-        </Typography>
-        {description}
-        {responsibilities}
-      </CardContent>
-      <CardActions>
-        <Button size="medium">Save</Button>
-        {applyButton}
-      </CardActions>
-    </Card>
+      {mockedJobs.map((job, index) => {
+        return <Card className={classes.card}>
+          <CardContent>
+            {close}
+            <Typography className={classes.title} variant="h5" color="textSecondary" gutterBottom>
+              {job.title}
+            </Typography>
+            <Typography color="textSecondary">
+              {job.city}, {job.state}
+            </Typography>
+            <Typography component="p">
+              {job.description}
+            </Typography>
+            {description}
+            {responsibilities}
+          </CardContent>
+          <CardActions>
+            <Button size="medium">Save</Button>
+            {applyButton}
+          </CardActions>
+        </Card>
+      })}
     </div>
   );
 }
---
+++ src/mocks/mockedJobs.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    jobId: 1,
+    companyId: 1,
+    description: "Seeking a Nuclear Pathologist Assistant with minimal experience and MRI imaging certifications.",
+    skills: ["bread", "carrots", "soup"],
+    title: "Nuclear Pathologist Assistant 1",
+    city: "New York City",
+    state: "NY",
+    active: true,
+    region: "Northeast",
+    type: "FullTime",
+    qualifications: "can cook"
+  },
+  {
+    jobId: 2,
+    companyId: 1,
+    description: "Seeking a Nuclear Pathologist Assistant with minimal experience and MRI imaging certifications.",
+    skills: ["bread", "carrots", "soup"],
+    title: "Nuclear Pathologist Assistant 2",
+    city: "New York City",
+    state: "NY",
+    active: true,
+    region: "Northeast",
+    type: "FullTime",
+    qualifications: "can cook"
+  },
+  {
+    jobId: 3,
+    companyId: 1,
+    description: "Seeking a Nuclear Pathologist Assistant with minimal experience and MRI imaging certifications.",
+    skills: ["bread", "carrots", "soup"],
+    title: "Nuclear Pathologist Assistant 3",
+    city: "New York City",
+    state: "NY",
+    active: true,
+    region: "Northeast",
+    type: "FullTime",
+    qualifications: "can cook"
+  }
+];
