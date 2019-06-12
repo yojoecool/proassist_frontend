c9005444acc4054dcb19a4aa62832ed483cc276b
yojoecool <yojoecool@gmail.com>
2019-06-12 19:36:47 -0400

    can't view profile without being logged in

--- src/pages/FileTests.jsx
+++ src/pages/FileTests.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { FileUpload, PdfViewer } from '../components';
+import { decodeToken, toast } from '../modules';
 
-function FileTests() {
-  return (
-    <React.Fragment>
-      <FileUpload />
-      <PdfViewer userId="546ec3f6-67cf-44a2-9054-ba0d8cb7c88c" />
-    </React.Fragment>
-  );
+function FileTests(props) {
+  if (!decodeToken()) {
+    toast('You must be logged in to view this page', 'error');
+    props.history.replace('/login');
+  } else {
+    return (
+      <React.Fragment>
+        <FileUpload />
+        <PdfViewer userId="546ec3f6-67cf-44a2-9054-ba0d8cb7c88c" />
+      </React.Fragment>
+    );
+  }
+
+  return <div />;
 }
 
-export default FileTests;
+export default withRouter(FileTests);
