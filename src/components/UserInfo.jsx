d1454349ece5b21f03f93f1ce2f59336980245ee
yojoecool <yojoecool@gmail.com>
2019-06-23 16:55:44 -0400

    pass userId to UserInfo

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import useLocalStorage from 'react-use-localstorage';
 import { useToken } from '../hooks';
+import { toast } from '../modules';
 
 function UserInfo(props) {
   const { userType, userId } = useToken();
@@ -12,16 +13,34 @@ function UserInfo(props) {
   React.useEffect(() => {
     const getUserInfo = async () => {
       const { REACT_APP_BACKEND_URL } = process.env;
+      const { userId: propsUserId } = props;
 
-      const { data } = await axios.get(
-        `${REACT_APP_BACKEND_URL}/users/userInfo`,
-        {
-          params: { user: userId },
-          headers: { authorization: `Bearer ${token}` }
+      let user = userId;
+      if (propsUserId) {
+        user = propsUserId;
+      }
+
+      try {
+        const { data } = await axios.get(
+          `${REACT_APP_BACKEND_URL}/users/userInfo`,
+          {
+            params: { user },
+            headers: { authorization: `Bearer ${token}` }
+          }
+        );
+
+        setUserData(data);
+      } catch (err) {
+        if (err.response.status === 404) {
+          toast('User not found', 'error');
+          return;
+        } else if (err.response.status === 403) {
+          toast('You are not authorized to view this user\'s info', 'error');
+          return;
         }
-      );
 
-      setUserData(data);
+        toast('Error loading user', 'error');
+      }
     };
 
     getUserInfo();
