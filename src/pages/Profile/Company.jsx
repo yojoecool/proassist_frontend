59652331108d6ec9a6f70644f62b24d85ec3ea59
adamlui <wingfului@gmail.com>
2019-07-09 23:24:04 -0400

    added loading circles and text during axios requests on company page

--- src/pages/Profile/Company.jsx
+++ src/pages/Profile/Company.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useLocalStorage from 'react-use-localstorage';
 import axios from 'axios';
 import {
-  Typography, Button
+  Typography, Button, CircularProgress
 } from '@material-ui/core';
 import { Link, withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -74,6 +74,9 @@ const useStyles = makeStyles(theme => ({
         justifyContent: 'center'
       },
   },
+  buttonProgress: {
+    color: theme.palette.secondary.main,
+  },
   center: {
     justifyContent: 'center'
   }
@@ -100,9 +103,13 @@ function Company(props) {
   const [offset, incrementOffset] = React.useState(0);
   const limit = 5;
 
+  const [profileLoading, setPLoading] = React.useState(false);
+  const [jobLoading, setJLoading] = React.useState(false);
+
   React.useEffect(() => {
     const getProfile = async () => {
       try {
+        setPLoading(true);
         const response = await axios.get(
           `${process.env.REACT_APP_BACKEND_URL}/companies/getProfile`, 
           { 
@@ -124,6 +131,8 @@ function Company(props) {
       } catch (err) {
         console.log(err)
         toast('Unable to load profile', 'error');
+      } finally {
+        setPLoading(false);
       }
     };
     getProfile();
@@ -132,6 +141,7 @@ function Company(props) {
   React.useEffect(() => {
     const getJobs = async () => {
       try {
+        setJLoading(true);
         const response = await
           axios.get(`${process.env.REACT_APP_BACKEND_URL}/companies/getJobs`,
             {
@@ -150,6 +160,8 @@ function Company(props) {
       } catch (err) {
         console.log(err);
         toast('Unable to load jobs', 'error');
+      } finally {
+        setJLoading(false);
       }
     };
 
@@ -163,43 +175,61 @@ function Company(props) {
     companyStatusMessage = 'Your profile has been blocked from posting jobs. Please reach out to the Admin.'
   } else if (userInfo.companyStatus === 'Active') {
     companyStatusMessage = 'Your profile has been approved to post jobs.'
+  } else if (profileLoading) {
+    companyStatusMessage = 'Loading...'
   } else {
     companyStatusMessage = '[Could not fetch profile details]'
   }
 
+  const loadingCircle = (isLoading) => {
+    if (isLoading) {
+      return (<CircularProgress size={24} className={classes.buttonProgress} />)
+    } else {
+      return <React.Fragment/>
+    }
+  }
+  const loadingText = (isLoading) => {
+    if (isLoading) {
+      return (<React.Fragment> Loading... </React.Fragment>)
+    } else {
+      return <React.Fragment/>
+    }
+  }
+
   return (
     <div className={classes.root}>
-      <Typography variant='h4' className={classes.header}>Welcome {userInfo.companyName}</Typography>
+      <Typography variant='h4' className={classes.header}>Welcome {userInfo.companyName} {loadingCircle(profileLoading)} </Typography>
       <div className={classes.content}>
 
         <div className={classes.subcontent}>
           <Typography variant='h5' className={classes.subheader}>Profile Information:</Typography>
           <p> Company/Login Email: {email} </p>
           <div className={classes.buttonDiv}> 
-            <Button size="large" variant="contained" component={Link} to='/profile/password' className={classes.button}>
+            <Button size="large" variant="contained" component={Link} to='/profile/password' className={classes.button} disabled={profileLoading}>
                 Change Password
             </Button>
           </div>
 
-          <Typography variant='h5' className={classes.subheader}>Point of Contact Information:</Typography>
-          <p> Name: {userInfo.firstName} {userInfo.lastName} </p>
-          <p> Email: {userInfo.pocEmail} </p>
-          <p> Phone Number: {userInfo.phoneNumber} </p>
+          <Typography variant='h5' className={classes.subheader}>Point of Contact Information: {loadingCircle(profileLoading)} </Typography>
+          <p> Name: {userInfo.firstName} {userInfo.lastName} {loadingText(profileLoading)} </p>
+          <p> Email: {userInfo.pocEmail} {loadingText(profileLoading)} </p>
+          <p> Phone Number: {userInfo.phoneNumber} {loadingText(profileLoading)}</p>
           <div className={classes.buttonDiv}> 
-            <Button size='large' variant='contained' component={Link} to='/profile/edit' className={classes.button}>
+            <Button size='large' variant='contained' component={Link} to='/profile/edit' className={classes.button} disabled={profileLoading}>
                 Edit Profile
             </Button>
           </div>
 
-          <Typography variant='h5' className={classes.subheader}>Company Status: {userInfo.companyStatus}</Typography>
+          <Typography variant='h5' className={classes.subheader}>Company Status: {userInfo.companyStatus} {loadingCircle(profileLoading)}</Typography>
           <p> {companyStatusMessage} </p>
 
         </div>
 
         <div className={classes.subcontent}>
-          <Typography variant='h5' className={classes.subheader}>Your Jobs: </Typography>
+          <Typography variant='h5' className={classes.subheader}>Your Jobs: {loadingCircle(jobLoading)} </Typography>
           <div className={classes.jobList}>
             <JobListing jobs={jobs} />
+            {loadingText(jobLoading)}
           </div>
           
           <div className={classNames(classes.buttonDiv, classes.center)}> 
@@ -208,12 +238,12 @@ function Company(props) {
               variant='contained'
               className={classes.button}
               onClick={() => incrementOffset(currOffset => currOffset + limit)}
-              disabled={!moreJobs}
+              disabled={!moreJobs || jobLoading}
             >
               See More Jobs
             </Button>
             <Button size='large' variant='contained' color='primary' component={Link} to='/profile/addjob' 
-              className={classes.button} disabled={userInfo.companyStatus!=='Active'}>
+              className={classes.button} disabled={userInfo.companyStatus!=='Active' || jobLoading}>
             Add New Job
             </Button>
           </div>
