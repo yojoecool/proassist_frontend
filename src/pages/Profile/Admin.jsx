e5a5914c0f37db0b72bd94801b7b7c834a8c1e2b
yojoecool <bjp0008@auburn.edu>
2019-12-08 17:23:04 -0500

    add button for creat job

--- src/pages/Profile/Admin.jsx
+++ src/pages/Profile/Admin.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 function Admin() {
   return (
     <React.Fragment>
-      <div>Admin</div>
+      <Button
+        size="medium"
+        component={Link}
+        to="/careers/addJob"
+        color="primary"
+      >
+        Create Job
+      </Button>
     </React.Fragment>
   );
 }
